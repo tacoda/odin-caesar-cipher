8ca30d39da53b50650d501188462f3c3502c40ef
tacoda <tacoda@hey.com>
2021-11-22 18:27:00 -0500

    Implement caesar cipher

--- caesar.rb
+++ caesar.rb
@@ -1,5 +1,12 @@
-def caesar_cipher(phrase, offset)
-  phrase
+class String
+  LOWER = ("a".."z").to_a
+  UPPER = ("A".."Z").to_a
+ 
+  def caesar_cipher(num)
+    self.tr(LOWER.join, LOWER.rotate(num).join)
+        .tr(UPPER.join, UPPER.rotate(num).join)
+  end
+ 
 end
 
-puts caesar_cipher("What a string!", 5)
+puts "What a string!".caesar_cipher(5)
